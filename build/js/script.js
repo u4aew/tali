3db96ec490dad6fd9b933a408c5ffe4720cc67dd
u4aew <dialoggg1994@gmail.com>
2016-10-11 14:53:43 +0700

    fixed

--- build/js/script.js
+++ build/js/script.js
@@ -3,3 +3,13 @@ $(document).ready(function () {
         $(".js-mobile-menu-wrap").toggleClass("js-mobile-menu-wrap-full");
     })
 });
+$(document).ready(function () {
+    $(".fancybox").fancybox();
+});$(document).ready(function () {
+    $(".js-mobile-menu").on('click', function () {
+        $(".js-mobile-menu-wrap").toggleClass("js-mobile-menu-wrap-full");
+    })
+});
+$(document).ready(function () {
+    $(".fancybox").fancybox();
+});
