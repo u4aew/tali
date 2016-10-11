69f08cb0bdcface114facb4c91f1d65198a40683
u4aew <dialoggg1994@gmail.com>
2016-10-11 16:11:21 +0700

    fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
     connect = require('gulp-connect'),
-    sass = require('gulp-sass');
-
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    jsmin = require('gulp-jsmin'),
+    rename = require('gulp-rename'),
+    cssmin = require('gulp-cssmin');
 gulp.task('default', ['connect', 'watch']);
 
 gulp.task('connect', function () {
@@ -13,6 +16,8 @@ gulp.task('connect', function () {
 gulp.task('watch', function () {
     gulp.watch(['index.html'], ['html']);
     gulp.watch(['src/style/sass/*.scss'], ['sass']);
+    gulp.watch(['src/style/css/*.css'], ['production-css']);
+    gulp.watch(['src/js/*.js'], ['production-js']);
 
 });
 gulp.task('html', function () {
@@ -22,7 +27,21 @@ gulp.task('html', function () {
 gulp.task('sass', function () {
     return gulp.src('src/style/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('build/style/'))
+        .pipe(gulp.dest('src/style/css/'))
         .pipe(connect.reload());
 });
- 
+
+gulp.task('production-js', function () {
+    return gulp.src(['src/js/jquery.js', 'src/js/jquery.popup.min.js', 'src/js/jquery.fancybox.js', 'src/js/script.js'])
+        .pipe(concat('all.js'))
+        .pipe(jsmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('build/js/'));
+});
+gulp.task('production-css', function () {
+    gulp.src(['src/style/css/normalize.css', 'src/style/css/bootstrap.css', 'src/style/css/bootstrap-normalize.css', 'src/style/css/jquery.fancybox.css', 'src/style/css/popup.css', 'src/style/css/style.css'])
+        .pipe(concat('style.css'))
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('build/style/'));
+});
